ccddab20a24f8ed97efeb79f43eab4aaea5cef0a
Ville Valkonen <weezelding@gmail.com>
2015-06-21 20:40:13 +0300

    Not sure what happened but lxml.html wasn't imported, import it.

--- Youtube/plugin.py
+++ Youtube/plugin.py
@@ -14,6 +14,7 @@ import supybot.plugins as plugins
 import supybot.ircutils as ircutils
 import supybot.callbacks as callbacks
 
+import lxml.html
 import re
 import requests
 import sqlite3 as sqlite
